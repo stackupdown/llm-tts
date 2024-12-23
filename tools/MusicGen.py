5bc289698f908b9e7f8bb838007e68a59c05d513
cbb38 <163497542+cbb38@users.noreply.github.com>
2024-12-23 16:01:22 +0800

    Add files via upload

--- tools/MusicGen.py
+++ tools/MusicGen.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# input: output.json
-# 生成的音乐.wav文件名最后的数字是output.json文件中的”pid“字段，也就是每句话的ID
-# 还有一个总体的背景音乐，提取output.json的music中出现次数最多的单词生成的一段长音乐（28s）
-# 时长不够应该可以循环
+# In[1]:
 
 
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
 
 
-# In[34]:
+# In[2]:
 
 
 from IPython.display import Audio
@@ -19,42 +16,38 @@ from collections import Counter
 import numpy as np
 
 
-# In[20]:
+# In[3]:
 
 
 import json
 
 
-# In[7]:
+# In[4]:
 
 
 processor = AutoProcessor.from_pretrained("facebook/musicgen-large")
 model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-large")
 
 
-# In[48]:
+# In[ ]:
 
 
 with open('output.json', 'r', encoding='utf-8') as json_file:
     items = json.load(json_file)
-
+    
+pid_character_count = {}
 music_style = []
-sampling_rate = model.config.audio_encoder.sampling_rate
 for item in items:
+    pid = item['pid']
+    text = item['text']
     music_prompt = item['music']
-    if music_prompt == '' or music_prompt == '无':
-        continue
-    music_style.append(music_prompt)
-    inputs = processor(text=["50s"+music_prompt], padding=True, return_tensors="pt")
+    text_length = len(text)
+    if pid in pid_character_count:
+        pid_character_count[pid] += text_length
+    else:
+        pid_character_count[pid] = text_length
 
-    audio_values = model.generate(**inputs, max_new_tokens=256)
-
-    audio_file_path = f"./music/generated_music_{item['pid']}.wav" 
-    scipy.io.wavfile.write(audio_file_path, rate=sampling_rate, data=audio_values[0, 0].numpy())
-
-
-# In[41]:
-
+        music_style.append(music_prompt)
 
 words = [word.strip() for phrase in music_style for word in phrase.split(',')]
 word_counts = Counter(words)
@@ -62,6 +55,28 @@ word_counts = Counter(words)
 most_common = word_counts.most_common(1)
 most_common_word = [word for word, count in most_common][0]
 print(most_common_word)
+
+
+# In[14]:
+
+
+sampling_rate = 22050
+for item in items:
+    print(1)
+    music_prompt = item['music']
+    pid = item['pid']
+    duration = pid_character_count[pid] * 0.3 # one character: 0.3s
+    if duration > 30:
+        duration = 30
+    if music_prompt == '' or music_prompt == '无' or most_common_word in music_prompt or duration < 10:
+        continue
+    inputs = processor(text=["50s"+music_prompt], padding=True, return_tensors="pt")
+
+    max_tokens = int(22050 * duration / 617)
+    audio_values = model.generate(**inputs, max_new_tokens=max_tokens)
+
+    audio_file_path = f"./music/generated_music_{item['pid']}_{int(duration)}s.wav" 
+    scipy.io.wavfile.write(audio_file_path, rate=sampling_rate, data=audio_values[0, 0].numpy())
 
 
 # In[62]:
@@ -86,6 +101,21 @@ Audio(audio_values[0].numpy(), rate=sampling_rate)
 
 
 scipy.io.wavfile.write("./music/total_musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
+
+
+# In[3]:
+
+
+jupyter nbconvert --to script MusicGen.ipynb
+
+
+# In[2]:
+
+
+import nbconvert
+
+
+# In[ ]:
 
 
 
