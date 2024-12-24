203becf7304b4850d227143a1a0c866b81668ed6
cbb38 <163497542+cbb38@users.noreply.github.com>
2024-12-24 11:40:21 +0800

    Add files via upload

--- tools/MusicGen.py
+++ tools/MusicGen.py
@@ -1,36 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
 
 from transformers import AutoProcessor, MusicgenForConditionalGeneration
-
-
-# In[2]:
-
 
 from IPython.display import Audio
 import scipy
 from collections import Counter
 import numpy as np
 
-
-# In[3]:
-
-
 import json
-# import torch
-
-
-# In[4]:
 
 
 processor = AutoProcessor.from_pretrained("facebook/musicgen-large")
 model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-large")
-
-
-# In[5]:
 
 
 with open('output_AddID.json', 'r', encoding='utf-8') as json_file:
@@ -58,15 +41,12 @@ most_common_word = [word for word, count in most_common][0]
 print(most_common_word)
 
 
-# In[ ]:
-
-
 sampling_rate = 22050
 for item in items:
     
     music_prompt = item['music']
     pid = item['pid']
-    print(pid)
+
     duration = pid_duration[pid]
     if duration > 30:
         duration = 30
@@ -81,9 +61,6 @@ for item in items:
     scipy.io.wavfile.write(audio_file_path, rate=sampling_rate, data=audio_values[0, 0].numpy())
 
 
-# In[62]:
-
-
 inputs = processor(text=["500s "+most_common_word+"and smooth and diverse background music for the play"], padding=True, return_tensors="pt")
 max_new_tokens = 1000
 audio_values = model.generate(**inputs, max_new_tokens=max_new_tokens)
@@ -93,12 +70,31 @@ audio_data = (audio_values[0, 0].numpy() * 32767).astype(np.int16)
 sampling_rate = 22050
 
 
-Audio(audio_values[0].numpy(), rate=sampling_rate)
 
+
+Audio(audio_values[0].numpy(), rate=sampling_rate)
 
 
 scipy.io.wavfile.write("./music/total_musicgen_out.wav", rate=sampling_rate, data=audio_values[0, 0].numpy())
 
+
+sampling_rate = 22050
+for item in items:
+    
+    music_prompt = item['sound']
+    audio_id = item['audio_id']
+    duration = item['audio_time_length']
+    if duration > 30:
+        duration = 30
+    if music_prompt == '' or music_prompt == '无' :
+        continue
+    inputs = processor(text=["50s"+music_prompt], padding=True, return_tensors="pt")
+
+    max_tokens = int(22050 * duration / 617)
+    audio_values = model.generate(**inputs, max_new_tokens=max_tokens)
+
+    audio_file_path = f"./audio/{item['audio_id']}.wav" 
+    scipy.io.wavfile.write(audio_file_path, rate=sampling_rate, data=audio_values[0, 0].numpy())
 
 
 
